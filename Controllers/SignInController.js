1b9a7ad4aac5729bcd8fece63c24bf3fa37a1b1b
FurkanYahsi <furkanyahsi103@gmail.com>
2025-06-26 11:09:16 +0300

    Some HTTP status codes are changed. Password validation process is corrected.

--- Controllers/SignInController.js
+++ Controllers/SignInController.js
@@ -5,29 +5,36 @@ const JWT_SECRET = process.env.JWT_SECRET || "default";
 
 exports.signin = async (req, res) => {
   try {
-    const { EmailorUsername, Password } = req.body;
+    const { EmailorUsername, Password } = req.body; // Password burada hashlenmiş olarak geliyor!
     const user = await UserService.getUserByEmailOrUsername(EmailorUsername);
 
     if (!user) {
-      return res.status(401).send("This email or username does not match with password.");
+      return res.status(400).send("This email or username does not match with password.");
     }
 
-    const isPasswordValid = await compare(Password, user.password);
-
-    if (!isPasswordValid) {
-      return res.status(401).send("This email or username does not match with password.");
+    if (Password !== user.password) {
+      return res.status(400).send("This email or username does not match with password.");
     }
 
-    const token = jwt.sign({ id: user.id, username: user.username }, JWT_SECRET, { expiresIn: '1s' });
+    const token = jwt.sign({ id: user.id, username: user.username }, JWT_SECRET, { expiresIn: '1h' });
 
     res.cookie('access_token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
-      sameSite: 'Strict', // Adjust as necessary for your application
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'Strict',
       maxAge: 3600000 // 1 hour
-      });
+    });
 
-    res.status(200).send({ message: "Sign in successful!", user: { id: user.id, name: user.name, surname: user.surname, username: user.username, email: user.email } });
+    res.status(200).send({
+      message: "Sign in successful!",
+      user: {
+        id: user.id,
+        name: user.name,
+        surname: user.surname,
+        username: user.username,
+        email: user.email
+      }
+    });
   } catch (err) {
     res.status(500).send("An error occurred during sign in.");
   }
