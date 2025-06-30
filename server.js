ba2d5fc6593236e27c8bef1afda9cf65a145463c
FurkanYahsi <furkanyahsi103@gmail.com>
2025-06-30 10:34:47 +0300

    Backend and Frontend are separately running. No need to build react app.

--- server.js
+++ server.js
@@ -1,23 +1,21 @@
-var express = require('express');
-var server = express();
-var cors = require('cors');
-var path = require('path');
-var cookieParser = require('cookie-parser');
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
 
+const server = express();
+
+// Middleware
 server.use(express.json()); // To parse JSON. If it's not included, req.body will be undefined.
-server.use(cors({
-      origin: 'http://localhost:3000',
-      credentials: true // Allow cookies to be sent
-}));
-server.use(cookieParser()); 
+server.use(cookieParser());
 
+// CORS setting: Accept requests from the React development server
+server.use(cors({
+  origin: 'http://localhost:3000', // React dev server address
+  credentials: true                // Allow cookies to be sent
+}));
+
+// Routes
 const authRoutes = require('./Routes/Auth');
 server.use('/auth', authRoutes);
 
-server.use(express.static(path.join(__dirname, '../My-Recipes-Frontend/build')));
-
-server.use(function (req, res, next) {
-   res.sendFile(path.join(__dirname, '../My-Recipes-Frontend/build', 'index.html'));
-});
-
-server.listen(3000);
+server.listen(3001);
