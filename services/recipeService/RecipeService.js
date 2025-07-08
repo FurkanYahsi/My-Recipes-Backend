f289a29d7d62ac5eea0afe9facfcb3ef3354f5d3
FurkanYahsi <furkanyahsi103@gmail.com>
2025-07-08 09:43:39 +0300

    Add method of RecipeService was written.

---
+++ services/recipeService/RecipeService.js
@@ -0,0 +1,8 @@
+const db_recipes = require('../../Databases/db_recipes');
+
+exports.createRecipe = async ({ recipe_name, recipe_ingredients, recipe_instructions, user_id}) => {
+    return db_recipes.query(
+        'INSERT INTO users (recipe_name, recipe_ingredients, recipe_instructions, user_id) VALUES ($1, $2, $3, $4) RETURNING *',
+        [recipe_name, recipe_ingredients, recipe_instructions, user_id]
+    );
+};
